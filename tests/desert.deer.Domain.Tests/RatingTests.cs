cfa391bc2403e5743dfd473da0ec07930d8302f1
MAbuShattal23 <abushattal@ieee.org>
2024-04-04 07:01:42 -0400

    Tested the first part ovf the lab (PP. 7)

--- tests/desert.deer.Domain.Tests/RatingTests.cs
+++ tests/desert.deer.Domain.Tests/RatingTests.cs
@@ -15,9 +15,19 @@ public class RatingTests
         Assert.AreEqual("Great fit!", rating.Review);
     }
 
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Cannot_Create_Rating_With_Invalid_UserName(){
+            var rating = new Rating(5, null, "Review");
+    } 
+
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void Cannot_Create_Rating_With_Invalid_Stars(){
             var rating = new Rating(0, "Mike", "Great fit!");
-    }
+    } 
+
+
+
 }
